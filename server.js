12c77cbbf4e8ab0177a732580276d3e1cf6143c2
mishraanmol <anmolmishra54@outlook.com>
2018-01-26 15:09:59 +0530

    Added a few of improvements

--- server.js
+++ server.js
@@ -4,8 +4,18 @@ var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 user =[];
 connections = [];
-server .listen(process.env.PORT || 3000);
+server .listen(process.env.PORT || 3900);
 console.log('Server running...');
 app.get('/',function(req,res){
 	res.sendFile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection',function (socket) {
+	connections.push(socket);
+	console.log('Connected: %s sockets Connected',connections.length());
+	
+	//Disconnect
+	connections.splice(connections.indexOf(socket),1);
+	console.log('Disconnected: %s sockets connected',connections.length());
+
 });
